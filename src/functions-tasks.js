b842b9523b8c9094c1d88963a2ab3d7fac9769d7
Ilya Spirkin <spirkinilya@gmail.com>
2024-04-11 20:48:00 +0300

    feat: retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -157,8 +157,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function fn(...args) {
+    let lastError;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func.apply(this, ...args);
+      } catch (error) {
+        lastError = error;
+      }
+    }
+    return lastError;
+  };
 }
 
 /**
